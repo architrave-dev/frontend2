4bd5688a27663054376df9ca9cbd00525cebaff2
youngdong82 <nfs82young@gmail.com>
2024-08-04 19:54:14 +0900

    feat: create default router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
+import Gnb from './component/gnb/Gnb';
+import Landing from './page/Landing';
+import Projects from './page/Projects';
+import About from './page/About';
 
 const App: React.FC = () => {
   return (
     <AppComp className="App">
+      <Router>
+        <Gnb />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </AppComp>
   );
 }
---
+++ src/page/ProjectDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectDetails: React.FC = () => {
+  return (
+    <div>
+      <h1>ProjectDetails Page</h1>
+    </div>
+  );
+}
+
+export default ProjectDetails;
