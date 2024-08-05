532437a80a0faedd81fc44321dbb98b5dcc049c5
youngdong82 <nfs82young@gmail.com>
2024-08-05 12:34:51 +0900

    feat: create ProjectSimple

---
+++ src/component/project/ProjectSimple.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import projectImg from '../../asset/project/starship.jpeg';
+
+const ProjectSimple: React.FC = () => {
+  return (
+    <ProjectSimpleComp>
+      <ProjectSimpleInfo>
+        <ProjectSimpleTitle>Project Title</ProjectSimpleTitle>
+        <ProjectSimpleDescription>This is Project description.This is Project description.This is Project description.</ProjectSimpleDescription>
+      </ProjectSimpleInfo>
+      <ProjectRepresent />
+    </ProjectSimpleComp>
+  );
+};
+
+const ProjectSimpleComp = styled.div`
+  width: 100%;
+  height: 56vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; 
+  margin-bottom: 20px;
+  background-color: #EECFBB;
+`;
+
+const ProjectSimpleInfo = styled.div`
+  width: calc(34vw);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
+  padding: 20px;
+`;
+const ProjectSimpleTitle = styled.h2`
+  margin-bottom: 10px;
+  text-align: end;
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  font-size: ${({ theme }) => theme.fontSize.font_H02};
+`;
+
+const ProjectSimpleDescription = styled.div`
+  text-align: end;
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  font-size: ${({ theme }) => theme.fontSize.font_B01};
+`;
+
+const ProjectRepresent = styled.div`
+  width: calc(66vw);
+  height: 100%;
+
+  background-image: url(${projectImg});
+  background-size: cover;
+  background-position: center;
+`;
+
+
+export default ProjectSimple;
