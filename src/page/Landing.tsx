b7ce016665ff3b4627255de56b5aafc12531c460
youngdong82 <nfs82young@gmail.com>
2024-08-06 12:39:59 +0900

    feat: import SearchBar, LandingLogo in Landing page

--- src/page/Landing.tsx
+++ src/page/Landing.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import SearchBar from '../component/SearchBar';
+import LandingLogo from '../component/LandingLogo';
 
 const Landing: React.FC = () => {
+  const handleSearch = (query: string) => {
+    console.log('Searching for:', query);
+  };
+
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <LandingContainer>
+      <LandingLogo />
     </div>
+      <SearchBar onSearch={handleSearch} />
+    </LandingContainer>
+
   );
 }
+
+const LandingContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default Landing;
