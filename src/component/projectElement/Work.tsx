ef299167e307d8ba27c32c1cc6b92a6e10fc8da4
youngdong82 <nfs82young@gmail.com>
2024-08-10 03:14:56 +0900

    feat: create editmode in TextBox, Work

--- src/component/projectElement/Work.tsx
+++ src/component/projectElement/Work.tsx
@@ -1,18 +1,116 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
+import { useAuthStore } from '../../shared/store';
 
 export interface WorkProps {
   image: string;
   title: string;
   description: string;
+  material: string;
+  size: string;
+  prodYear: number;
 }
 
-const Work: React.FC<WorkProps> = ({ image, title, description }) => {
+const Work: React.FC<WorkProps> = ({
+  image: initialImage,
+  title: initialTitle,
+  description: initialDescription,
+  material: initialMaterial,
+  size: initialSize,
+  prodYear: initialProdYear
+}) => {
+  const isEditMode = useAuthStore((state) => state.isEditMode);
+  const [image, setImage] = useState(initialImage);
+  const [title, setTitle] = useState(initialTitle);
+  const [description, setDescription] = useState(initialDescription);
+  const [material, setMaterial] = useState(initialMaterial);
+  const [size, setSize] = useState(initialSize);
+  const [prodYear, setProdYear] = useState(initialProdYear);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageClick = () => {
+    if (isEditMode && fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const triggerFileInput = () => {
+    fileInputRef.current?.click();
+  };
+
+
   return (
     <WorkWrapper>
-      <WorkImage src={image} alt={title} />
-      <WorkTitle>[{title}]</WorkTitle>
-      <WorkDescription>{description}</WorkDescription>
+      {isEditMode ? (
+        <>
+          <ImgWrapper>
+            <WorkImage src={image} alt={title} onClick={handleImageClick} />
+            <ReplaceImageButton onClick={triggerFileInput}>
+              이미지 교체
+            </ReplaceImageButton>
+            <HiddenFileInput
+              type="file"
+              ref={fileInputRef}
+              onChange={handleImageChange}
+              accept="image/*"
+            />
+          </ImgWrapper>
+          <TitleTextAreaWrpper>
+            <TitleInput
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+            />
+            <Textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description"
+            />
+          </TitleTextAreaWrpper>
+          <WorkInfoEdit>
+            <Input
+              value={material}
+              onChange={(e) => setMaterial(e.target.value)}
+              placeholder="Material"
+            />
+            <Input
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+              placeholder="Size"
+            />
+            <Input
+              type="number"
+              value={prodYear}
+              onChange={(e) => setProdYear(Number(e.target.value))}
+              placeholder="Year"
+            />
+          </WorkInfoEdit>
+        </>
+      ) : (
+        <>
+          <ImgWrapper>
+            <WorkImage src={image} alt={title} />
+          </ImgWrapper>
+          <Title>[{title}]</Title>
+          <Description>{description}</Description>
+          <WorkInfo>
+            <Material>{material},</Material>
+            <Size>{size},</Size>
+            <ProdYear>{prodYear}</ProdYear>
+          </WorkInfo>
+        </>
+      )}
     </WorkWrapper>
   );
 };
@@ -23,6 +121,9 @@ const WorkWrapper = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+const ImgWrapper = styled.div`
+  position: relative;
+`
 
 const WorkImage = styled.img`
   max-width: 100%;
@@ -31,7 +132,7 @@ const WorkImage = styled.img`
   object-fit: contain;
 `;
 
-const WorkTitle = styled.h2`
+const Title = styled.h2`
   width: 100%;
   padding: 0 calc(10vw);
   margin-bottom: 4px;
@@ -41,7 +142,27 @@ const WorkTitle = styled.h2`
   text-align: center;
 `;
 
-const WorkDescription = styled.p`
+const Description = styled.div`
+  width: 100%;
+  padding: 0 calc(10vw);
+  color: ${({ theme }) => theme.colors.color_Gray_04};
+  font-size: ${({ theme }) => theme.fontSize.font_B04};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  text-align: center;
+  margin-bottom: 4px;
+`
+const WorkInfoEdit = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between; 
+  padding: 0 calc(10vw);
+  color: ${({ theme }) => theme.colors.color_Gray_04};
+  font-size: ${({ theme }) => theme.fontSize.font_B04};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  text-align: center;
+`;
+
+const WorkInfo = styled.div`
   width: 100%;
   padding: 0 calc(10vw);
   color: ${({ theme }) => theme.colors.color_Gray_04};
@@ -50,5 +171,72 @@ const WorkDescription = styled.p`
   text-align: center;
 `;
 
+const Material = styled.span`
+  margin-right: 4px;
+`;
+const Size = styled.span`
+  margin-right: 4px;
+`;
+const ProdYear = styled.span``;
+
+const TitleTextAreaWrpper = styled.div`
+  width: 100%;
+  padding: 0 calc(10vw);
+`
+
+const TitleInput = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 8px;
+  font-size: ${({ theme }) => theme.fontSize.font_B04};
+  color: ${({ theme }) => theme.colors.color_Gray_04};
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.color_Gray_05};
+  outline: none;
+`;
+
+const Input = styled.input`
+  width: 200px;
+  padding: 8px;
+  margin-bottom: 8px;
+  text-align: center;
+  font-size: ${({ theme }) => theme.fontSize.font_B04};
+  color: ${({ theme }) => theme.colors.color_Gray_04};
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.color_Gray_05};
+  outline: none;
+`;
+
+const Textarea = styled.textarea`
+  width: 100%;
+  min-height: 100px;
+  padding: 8px;
+  margin-bottom: 8px;
+  font-size: ${({ theme }) => theme.fontSize.font_B04};
+  color: ${({ theme }) => theme.colors.color_Gray_04};
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.colors.color_Gray_05};
+  outline: none;
+  resize: vertical;
+`;
+
+const ReplaceImageButton = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+`;
+
+const HiddenFileInput = styled.input`
+  display: none;
+`;
 
 export default Work;
