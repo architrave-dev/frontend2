c82c3588ca80f2be437e301e4f654d2f39404962
youngdong82 <nfs82young@gmail.com>
2024-08-09 20:55:17 +0900

    feat: change Divider more flexible

--- src/shared/Divider.tsx
+++ src/shared/Divider.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export enum EnumType {
+export enum DividerType {
   PLAIN = 'Plain',
   DOTTED = 'Dotted',
   DASHED = 'Dashed',
 }
 
-interface DividerProps {
-  dividerType: EnumType;
+export interface DividerProps {
+  dividerType: DividerType;
 }
 
 const Divider: React.FC<DividerProps> = ({ dividerType }) => {
@@ -17,17 +17,19 @@ const Divider: React.FC<DividerProps> = ({ dividerType }) => {
   );
 }
 
-const DividerComp = styled.hr<{ dividerType: EnumType }>`
-  border-top: 1px solid;
-  border-color: ${({ theme }) => theme.colors.color_Alpha_02};
+const DividerComp = styled.hr<{ dividerType: DividerType }>`
+  display: block;
+  border-top: 1px;
+  transform: scaleY(0.3);
+  border-color: ${({ theme }) => theme.colors.color_Gray_05};
   margin-bottom: 40px;
   border-style: ${({ dividerType }) => {
     switch (dividerType) {
-      case EnumType.DOTTED:
+      case DividerType.DOTTED:
         return 'dotted';
-      case EnumType.DASHED:
+      case DividerType.DASHED:
         return 'dashed';
-      case EnumType.PLAIN:
+      case DividerType.PLAIN:
       default:
         return 'solid';
     }
