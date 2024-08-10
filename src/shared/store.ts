8e3eaa9e72fe384d3650d543071cd35aea63d43a
youngdong82 <nfs82young@gmail.com>
2024-08-11 02:23:09 +0900

    feat: add user and auth data in store

--- src/shared/store.ts
+++ src/shared/store.ts
@@ -5,26 +5,43 @@ export enum ModalType {
   LOGIN = 'LOGIN',
   ALERT = 'ALERT',
 }
+export interface UserData {
+  id: number;
+  email: string;
+  username: string;
+  role: string;
+}
 
 interface AuthState {
+  user: UserData | null;
+  authToken: string | null;
   isLoggedIn: boolean;
   isEditMode: boolean;
   modalType: ModalType;
 }
 
 interface AuthActions {
+  setUser: (user: UserData | null) => void;
+  setAuthToken: (token: string | null) => void;
   setIsLoggedIn: (value: boolean) => void;
   setIsEditMode: (value: boolean) => void;
   setModalType: (type: ModalType) => void;
+  clearAuth: () => void;
 }
 
 type AuthStore = AuthState & AuthActions;
 
 export const useAuthStore = create<AuthStore>()((set) => ({
+  user: null,
+  authToken: null,
   isLoggedIn: false,
   isEditMode: false,
   modalType: ModalType.NONE,
+
+  setUser: (user) => set({ user }),
+  setAuthToken: (authToken) => set({ authToken }),
   setIsLoggedIn: (value) => set({ isLoggedIn: value }),
   setIsEditMode: (value) => set({ isEditMode: value }),
   setModalType: (type) => set({ modalType: type }),
+  clearAuth: () => set({ user: null, authToken: null, isLoggedIn: false }),
 }));
