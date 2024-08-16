2b8f52d3135481304057baf876e35dafa0a9ab3f
youngdong82 <nfs82young@gmail.com>
2024-08-16 19:50:57 +0900

    feat: change function name in useProjectList

--- src/shared/hooks/useProjectList.ts
+++ src/shared/hooks/useProjectList.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ProjectSimpleData, useProjectListStore } from '../store/projectListStore';
-import { ProjectResponse, getProjectList } from '../api/projectApi';
+import { ProjectListResponse, ProjectResponse, getProjectList } from '../api/projectApi';
 
 
 interface UseProjectListResult {
@@ -10,26 +10,26 @@ interface UseProjectListResult {
   getProjectList: (data: string) => Promise<void>;
 }
 
-export const useProjectList = () => {
+export const useProjectList = (): UseProjectListResult => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const { projects, setProjects } = useProjectListStore();
 
-  const handleLandingBoxSuccess = (response: ProjectResponse) => {
+  const handleProjectListSuccess = (response: ProjectListResponse) => {
     const projectListData = response.data;
     console.log("projectListData from useProjectList: ", projectListData);
     setProjects(projectListData);
   };
 
   const handleProjectRequest = async <T extends string>(
-    projectFunction: (data: T) => Promise<ProjectResponse>,
+    projectFunction: (data: T) => Promise<ProjectListResponse>,
     data: T
   ) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await projectFunction(data);
-      handleLandingBoxSuccess(response);
+      handleProjectListSuccess(response);
     } catch (err) {
       setError(err instanceof Error ? err.message : 'An unexpected error occurred');
     } finally {
