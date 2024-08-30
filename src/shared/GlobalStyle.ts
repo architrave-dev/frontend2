a957a67b8745c37e432a0aa08bf55f669a63b741
youngdong82 <nfs82young@gmail.com>
2024-08-30 20:02:17 +0900

    feat: add Pretendard

--- src/shared/GlobalStyle.ts
+++ src/shared/GlobalStyle.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
-    font-family: var(--font-default, sans-serif);
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     margin: 0;
     padding: 0;
   }
