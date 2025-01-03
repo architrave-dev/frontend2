c2a5050134ddbfe74307711288fddb15d5b945f0
youngdong82 <nfs82young@gmail.com>
2025-01-03 19:39:50 +0900

    feat: create redirect logic in ErrorPage

--- src/page/Error.tsx
+++ src/page/Error.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-function ErrorPage() {
+const ErrorPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(3);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown(prev => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      navigate('/');
+    }
+  }, [countdown, navigate]);
+
   return (
-    <div>
-      <h1>Error: Invalid artist ID</h1>
-      <p>The artist ID provided does not match the required format.</p>
-    </div>
+    <ErrorPageComp>
+      <ErrorHead>Error: Invalid artist ID</ErrorHead>
+      <ErrorMessage>The provided artist ID does not match the required format.</ErrorMessage>
+      <ErrorMessage>Redirecting to home in {countdown} seconds.</ErrorMessage>
+    </ErrorPageComp>
   );
 }
 
 export default ErrorPage;
+
+const ErrorPageComp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const ErrorHead = styled.h2`
+  ${({ theme }) => theme.typography.Body_01_1}; 
+  margin-bottom: 1vh;
+`
+
+const ErrorMessage = styled.div`
+  ${({ theme }) => theme.typography.Body_02_2}; 
+`
