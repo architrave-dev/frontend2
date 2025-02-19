674674819c023e7cb3a8d5fb9d7b54d64edeaf33
youngdong82 <nfs82young@gmail.com>
2025-02-20 00:32:58 +0900

    feat: create new MoleculeInputDivWithAction

--- src/component/contact/Contact.tsx
+++ src/component/contact/Contact.tsx
@@ -10,12 +10,11 @@ import { AlertPosition, AlertType, ModalType } from '../../shared/enum/EnumRepos
 import { useMenu } from '../../shared/hooks/useMenu';
 import { useModalStore } from '../../shared/store/portal/modalStore';
 import { useStandardAlertStore } from '../../shared/store/portal/alertStore';
-import { useEditMode } from '../../shared/hooks/useEditMode';
+import MoleculeInputDivWithAction from '../../shared/component/molecule/MoleculeInputDivWithAction';
 
 
 const ContactComp: React.FC = () => {
   const { aui } = useAui();
-  const { isEditMode } = useEditMode();
   const { contact, getContact } = useContact();
   const { closeMenu } = useMenu();
   const { setStandardModal } = useModalStore();
@@ -69,17 +68,14 @@ const ContactComp: React.FC = () => {
           />
         </Contact_1>
         <Contact_1>
-          <MoleculeInputDiv
+          <MoleculeInputDivWithAction
             value={contact.email}
             placeholder={"email"}
             handleChange={(e) => handleChange({ email: e.target.value })}
+            onClick={handleSendEmail}
             inputStyle={ContactInput}
             StyledDiv={ContactDiv}
           />
-          {/* {contact.email !== '' &&
-            !isEditMode &&
-            <SendEmailButton onClick={handleSendEmail}>Send Email</SendEmailButton>
-          } */}
           <MoleculeInputDiv
             value={contact.contact}
             placeholder={"contact"}
@@ -162,38 +158,30 @@ const ContactTitle = styled.div`
 
 const ContactDiv = styled.div`
   width: 100%;
-  height: fit-content;
+  height: 20px;
 
-  padding: 2px 0px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 
   color: ${({ theme }) => theme.colors.color_Gray_02};
   ${({ theme }) => theme.typography.Body_02_2};
 `
 
 const ContactAnchor = styled.a`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-
   width: 20%;
   min-width: 100px;
   height: 20px;
 
-  padding: 2px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
   text-decoration: underline;
 
   color: ${({ theme }) => theme.colors.color_Gray_02};
   ${({ theme }) => theme.typography.Body_02_2};
   cursor: pointer;
 `
-
-const SendEmailButton = styled.button`
-  width: 100%;
-  height: 20px;
-  background-color: ${({ theme }) => theme.colors.color_Gray_02};
-  color: ${({ theme }) => theme.colors.color_White};
-`;
-
 
 export default ContactComp;
