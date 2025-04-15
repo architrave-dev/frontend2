270c4afbea55fb06646eae3b1c2251e480993b22
youngdong82 <nfs82young@gmail.com>
2025-04-15 18:34:12 +0700

    feat: create test S3Bucket & split it

--- src/shared/env/envManager.ts
+++ src/shared/env/envManager.ts
@@ -4,6 +4,7 @@ interface EnvironmentConfig {
   apiBaseUrl: string;
   s3BucketName: string;
   s3BucketDomain: string;
+  cloudFrontDomain: string;
 }
 
 const configs: Record<Environment, EnvironmentConfig> = {
@@ -11,11 +12,13 @@ const configs: Record<Environment, EnvironmentConfig> = {
     apiBaseUrl: 'http://localhost:8080',
     s3BucketName: process.env.REACT_APP_S3_BUCKET_NAME || '',
     s3BucketDomain: process.env.REACT_APP_BUCKET_DOMAIN || '',
+    cloudFrontDomain: process.env.REACT_APP_DOMAIN || '',
   },
   prod: {
     apiBaseUrl: process.env.REACT_APP_BACKEND_PROD_IP || '',
     s3BucketName: process.env.REACT_APP_S3_BUCKET_NAME_PROD || '',
     s3BucketDomain: process.env.REACT_APP_BUCKET_DOMAIN_PROD || '',
+    cloudFrontDomain: process.env.REACT_APP_TEST_DOMAIN || '',
   },
 };
 
